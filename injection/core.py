ab99d3d43194a17bd0ce15c9dcf76a249b362667
remimd <remimd@github.com>
2023-11-01 21:58:59 +0100

    on progress

--- injection/core.py
+++ injection/core.py
@@ -14,10 +14,12 @@ from typing import (
     Iterator,
     Mapping,
     NamedTuple,
+    Protocol,
     TypeVar,
     cast,
     final,
     get_origin,
+    runtime_checkable,
 )
 
 from injection.exceptions import NoInjectable
@@ -149,6 +151,64 @@ class Binder:
         return self
 
 
+@dataclass(repr=False, frozen=True, slots=True)
+class Manager:
+    __container: dict[type, Injectable] = field(default_factory=dict, init=False)
+    __binders: set[Binder] = field(default_factory=set, init=False)
+
+    @property
+    def inject(self) -> InjectDecorator:
+        return InjectDecorator(self)
+
+    @property
+    def injectable(self) -> InjectableDecorator:
+        return InjectableDecorator(self, NewInjectable)
+
+    @property
+    def singleton(self) -> InjectableDecorator:
+        return InjectableDecorator(self, SingletonInjectable)
+
+    def get(self, __reference: type) -> Injectable:
+        cls = origin if (origin := get_origin(__reference)) else __reference
+
+        try:
+            return self.__container[cls]
+        except KeyError as exc:
+            try:
+                name = cls.__name__
+            except AttributeError:
+                name = repr(__reference)
+
+            raise NoInjectable(f"No injectable for {name}.") from exc
+
+    def set_multiple(self, __references: Iterable[type], __injectable: Injectable):
+        new_values = (
+            (self.check_if_exists(reference), __injectable)
+            for reference in __references
+        )
+        self.__container.update(new_values)
+        self.__notify_binders()
+        return self
+
+    def check_if_exists(self, __reference: type) -> type:
+        if __reference in self.__container:
+            raise RuntimeError(
+                f"An injectable already exists for the "
+                f"reference class `{__reference.__name__}`."
+            )
+
+        return __reference
+
+    def new_binder(self, __callable: Callable[..., Any]) -> Binder:
+        binder = Binder(__callable).notify(self)
+        self.__binders.add(binder)
+        return binder
+
+    def __notify_binders(self):
+        for binder in self.__binders:
+            binder.notify(self)
+
+
 @final
 @dataclass(repr=False, frozen=True, slots=True)
 class InjectDecorator:
@@ -221,85 +281,24 @@ class InjectableDecorator:
         return decorator(wrapped) if wrapped else decorator
 
 
-@dataclass(repr=False, frozen=True, slots=True)
-class Manager:
-    __container: dict[type, Injectable] = field(default_factory=dict, init=False)
-    __binders: set[Binder] = field(default_factory=set, init=False)
-
-    @property
-    def inject(self) -> InjectDecorator:
-        return InjectDecorator(self)
-
-    @property
-    def injectable(self) -> InjectableDecorator:
-        return InjectableDecorator(self, NewInjectable)
-
-    @property
-    def singleton(self) -> InjectableDecorator:
-        return InjectableDecorator(self, SingletonInjectable)
-
-    def get(self, __reference: type) -> Injectable:
-        cls = origin if (origin := get_origin(__reference)) else __reference
-
-        try:
-            return self.__container[cls]
-        except KeyError as exc:
-            try:
-                name = cls.__name__
-            except AttributeError:
-                name = repr(__reference)
-
-            raise NoInjectable(f"No injectable for {name}.") from exc
-
-    def set_multiple(self, __references: Iterable[type], __injectable: Injectable):
-        new_values = (
-            (self.check_if_exists(reference), __injectable)
-            for reference in __references
-        )
-        self.__container.update(new_values)
-        self.__notify_binders()
-        return self
-
-    def check_if_exists(self, __reference: type) -> type:
-        if __reference in self.__container:
-            raise RuntimeError(
-                f"An injectable already exists for the "
-                f"reference class `{__reference.__name__}`."
-            )
-
-        return __reference
-
-    def new_binder(self, __callable: Callable[..., Any]) -> Binder:
-        binder = Binder(__callable).notify(self)
-        self.__binders.add(binder)
-        return binder
-
-    def __notify_binders(self):
-        for binder in self.__binders:
-            binder.notify(self)
-
-
-class InjectionModule(ABC):
-    @abstractmethod
-    def inject(self, *args, **kwargs):
-        raise NotImplementedError
-
-    @abstractmethod
-    def injectable(self, **kwargs):
-        raise NotImplementedError
-
-    @abstractmethod
-    def singleton(self, **kwargs):
-        raise NotImplementedError
-
-    @abstractmethod
+@runtime_checkable
+class Module(Protocol):
     def get_instance(self, *args, **kwargs):
-        raise NotImplementedError
+        ...
+
+    def inject(self, *args, **kwargs):
+        ...
+
+    def injectable(self, *args, **kwargs):
+        ...
+
+    def singleton(self, *args, **kwargs):
+        ...
 
 
 @dataclass(repr=False, frozen=True, slots=True)
-class _Module(InjectionModule):
-    __manager: Manager = field(default_factory=Manager)
+class InjectionModule:
+    __manager: Manager
 
     @property
     def inject(self) -> InjectDecorator:
@@ -318,22 +317,10 @@ class _Module(InjectionModule):
         return cast(reference, instance)
 
 
-def new_module() -> InjectionModule:
-    return _Module()
+def new_module() -> Module:
+    manager = Manager()
+    module = InjectionModule(manager)
+    return cast(Module, module)
 
 
-_default_module = new_module()
-
-get_instance = _default_module.get_instance
-inject = _default_module.inject
-injectable = _default_module.injectable
-singleton = _default_module.singleton
-
-del _default_module
-
-__all__ = (
-    "get_instance",
-    "inject",
-    "injectable",
-    "singleton",
-)
+__all__ = ("Module", "new_module")
--- injection/core.pyi
+++ injection/core.pyi
@@ -2,35 +2,44 @@ from typing import Any, Callable, Iterable, TypeVar, overload
 
 _T = TypeVar("_T")
 
-def get_instance(reference: type[_T]) -> _T:
-    """
-    Function used to retrieve an instance associated with the type passed in parameter or raise `NoInjectable`
-    exception.
-    """
+class Module:
+    def get_instance(self, reference: type[_T]) -> _T:
+        """
+        Function used to retrieve an instance associated with the type passed in parameter or raise `NoInjectable`
+        exception.
+        """
+    def inject(self, wrapped: Callable[..., Any] = ..., /):
+        """
+        Decorator applicable to a class or function. Inject function dependencies using parameter type annotations. If
+        applied to a class, the dependencies resolved will be those of the `__init__` method.
 
-def inject(wrapped: Callable[..., Any] = ..., /):
-    """
-    Decorator applicable to a class or function. Inject function dependencies using parameter type annotations. If
-    applied to a class, the dependencies resolved will be those of the `__init__` method.
+        Doesn't work with type annotations resolved by `__future__` module.
+        """
+    def injectable(self, *, reference: type = ..., auto_inject: bool = ...):
+        """
+        Decorator applicable to a class or function. It is used to indicate how the injectable will be constructed. At
+        injection time, a new instance will be injected each time. Automatically injects constructor dependencies, can be
+        disabled with `auto_inject=False`.
+        """
+    @overload
+    def injectable(
+        self,
+        *,
+        references: Iterable[type] = ...,
+        auto_inject: bool = ...,
+    ): ...
+    def singleton(self, *, reference: type = ..., auto_inject: bool = ...):
+        """
+        Decorator applicable to a class or function. It is used to indicate how the singleton will be constructed. At
+        injection time, the injected instance will always be the same. Automatically injects constructor dependencies, can be
+        disabled with `auto_inject=False`.
+        """
+    @overload
+    def singleton(
+        self,
+        *,
+        references: Iterable[type] = ...,
+        auto_inject: bool = ...,
+    ): ...
 
-    Doesn't work with type annotations resolved by `__future__` module.
-    """
-
-def injectable(*, reference: type = ..., auto_inject: bool = ...):
-    """
-    Decorator applicable to a class or function. It is used to indicate how the injectable will be constructed. At
-    injection time, a new instance will be injected each time. Automatically injects constructor dependencies, can be
-    disabled with `auto_inject=False`.
-    """
-
-@overload
-def injectable(*, references: Iterable[type] = ..., auto_inject: bool = ...): ...
-def singleton(*, reference: type = ..., auto_inject: bool = ...):
-    """
-    Decorator applicable to a class or function. It is used to indicate how the singleton will be constructed. At
-    injection time, the injected instance will always be the same. Automatically injects constructor dependencies, can be
-    disabled with `auto_inject=False`.
-    """
-
-@overload
-def singleton(*, references: Iterable[type] = ..., auto_inject: bool = ...): ...
+def new_module() -> Module: ...
