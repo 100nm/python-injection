a7483a06f7ffb8d053d2a0f1f2fc95593da59473
remimd <remimd@github.com>
2024-07-13 11:38:27 +0200

    feat: 🔧 PyPI Classifiers

--- tests/utils/test_load_package.py
+++ tests/utils/test_load_package.py
@@ -6,6 +6,21 @@ from injection.utils import load_package
 
 
 class TestLoadPackage:
+    def test_load_package_with_predicate(self):
+        from tests.utils import package
+
+        load_package(package, predicate=lambda name: ".excluded_package." not in name)
+
+        assert "tests.utils.package.excluded_package.module3" not in sys.modules
+
+        modules = (
+            "tests.utils.package.module1",
+            "tests.utils.package.sub_package.module2",
+        )
+
+        for module in modules:
+            assert module in sys.modules
+
     def test_load_package_with_success(self):
         from tests.utils import package
 
@@ -14,6 +29,7 @@ class TestLoadPackage:
         modules = (
             "tests.utils.package.module1",
             "tests.utils.package.sub_package.module2",
+            "tests.utils.package.excluded_package.module3",
         )
 
         for module in modules:
@@ -25,6 +41,7 @@ class TestLoadPackage:
         modules = (
             "tests.utils.package.module1",
             "tests.utils.package.sub_package.module2",
+            "tests.utils.package.excluded_package.module3",
         )
 
         for module in modules:
@@ -35,18 +52,3 @@ class TestLoadPackage:
 
         with pytest.raises(TypeError):
             load_package(module1)
-
-    def test_load_package_with_predicate(self):
-        from tests.utils import package
-
-        load_package(package, predicate=lambda name: ".excluded_package." not in name)
-
-        assert "tests.utils.excluded_package.module3" not in sys.modules
-
-        modules = (
-            "tests.utils.package.module1",
-            "tests.utils.package.sub_package.module2",
-        )
-
-        for module in modules:
-            assert module in sys.modules
