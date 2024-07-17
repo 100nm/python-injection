69174325cb9a89735d8989e568e66b4297d3e73b
remimd <remimd@github.com>
2024-07-17 18:45:20 +0200

    feat: ✨ Benchmark for `@inject`

--- bench.py
+++ bench.py
@@ -1,15 +1,103 @@
+import inspect
 import itertools
-from collections.abc import Callable, Iterable, Iterator
+from collections.abc import Callable, Iterator
 from dataclasses import dataclass
 from decimal import Decimal
 from statistics import mean
 from timeit import timeit
-from typing import Annotated, Any, Self
+from typing import Annotated, Any, ClassVar, Self
 
 from tabulate import tabulate
 from typer import Option, Typer
 
 from injection import inject, injectable
+
+
+@dataclass(frozen=True, slots=True)
+class Benchmark:
+    x: Decimal
+    y: Decimal
+
+    @property
+    def difference_rate(self) -> Decimal:
+        return ((self.y - self.x) / self.x) * 100
+
+    @classmethod
+    def compare(
+        cls,
+        x: Callable[..., Any],
+        y: Callable[..., Any],
+        number: int = 1,
+    ) -> Self:
+        x = mean(cls._time_in_ns(x, number))
+        y = mean(cls._time_in_ns(y, number))
+        return cls(x, y)
+
+    @staticmethod
+    def _time_in_ns(
+        callable_: Callable[..., Any],
+        /,
+        number: int,
+    ) -> Iterator[Decimal]:
+        for _ in range(number):
+            delta = timeit(callable_, number=1)
+            yield Decimal(delta) * (10**6)
+
+
+@dataclass(frozen=True, slots=True)
+class BenchmarkResult:
+    title: str
+    benchmark: Benchmark
+
+    @property
+    def row(self) -> tuple[str, str, str, str]:
+        rate = self.benchmark.difference_rate
+        return (
+            self.title,
+            f"{self.benchmark.x:.2f}μs",
+            f"{self.benchmark.y:.2f}μs",
+            f"{rate:.2f}% slower" if rate >= 0 else f"{abs(rate):.2f}% faster",
+        )
+
+
+@dataclass(frozen=True, slots=True)
+class InjectBenchmark:
+    callables: ClassVar[dict[str, Callable[..., Any]]] = {}
+
+    def start(self, number: int = 1) -> Iterator[BenchmarkResult]:
+        for title, callable_ in self.callables.items():
+            signature = inspect.signature(callable_, eval_str=True)
+            dependencies = {
+                name: parameter.annotation
+                for name, parameter in signature.parameters.items()
+            }
+
+            def reference():
+                return callable_(**{name: d() for name, d in dependencies.items()})
+
+            first = Benchmark.compare(
+                reference,
+                lambda: inject(callable_)(),
+                number=number,
+            )
+            yield BenchmarkResult(f"{title} (first run)", first)
+
+            injected = inject(callable_)
+            injected()
+            instance = Benchmark.compare(
+                reference,
+                injected,
+                number=number,
+            )
+            yield BenchmarkResult(title, instance)
+
+    @classmethod
+    def register(cls, wrapped: Callable[..., Any] = None, /, *, title: str):
+        def decorator(wp):
+            cls.callables[title] = wp
+            return wp
+
+        return decorator(wrapped) if wrapped else decorator
 
 
 @injectable
@@ -37,101 +125,34 @@ class E:
     pass
 
 
-def with_0_dependency():
+@InjectBenchmark.register(title="0 dependency")
+def function_with_0_dependency():
     pass
 
 
-def with_1_dependency(__a: A):
+@InjectBenchmark.register(title="1 dependencies")
+def function_with_1_dependency(__a: A):
     pass
 
 
-def with_2_dependencies(__a: A, __b: B):
+@InjectBenchmark.register(title="2 dependencies")
+def function_with_2_dependencies(__a: A, __b: B):
     pass
 
 
-def with_3_dependencies(__a: A, __b: B, __c: C):
+@InjectBenchmark.register(title="3 dependencies")
+def function_with_3_dependencies(__a: A, __b: B, __c: C):
     pass
 
 
-def with_4_dependencies(__a: A, __b: B, __c: C, __d: D):
+@InjectBenchmark.register(title="4 dependencies")
+def function_with_4_dependencies(__a: A, __b: B, __c: C, __d: D):
     pass
 
 
-def with_5_dependencies(__a: A, __b: B, __c: C, __d: D, __e: E):
+@InjectBenchmark.register(title="5 dependencies")
+def function_with_5_dependencies(__a: A, __b: B, __c: C, __d: D, __e: E):
     pass
-
-
-@dataclass(frozen=True, slots=True)
-class Benchmark:
-    x: Decimal
-    y: Decimal
-
-    @property
-    def difference_rate(self) -> Decimal:
-        return ((self.y - self.x) / self.x) * 100
-
-    @classmethod
-    def compare(
-        cls,
-        x: Callable[..., Any],
-        y: Callable[..., Any],
-        number: int = 1,
-    ) -> Self:
-        x = mean(cls.__time_in_ns(x, number))
-        y = mean(cls.__time_in_ns(y, number))
-        return cls(x, y)
-
-    @staticmethod
-    def __time_in_ns(
-        __callable: Callable[..., Any],
-        /,
-        number: int,
-    ) -> Iterator[Decimal]:
-        for _ in range(number):
-            delta = timeit(__callable, number=1)
-            yield Decimal(delta) * (10**6)
-
-
-@dataclass(frozen=True, slots=True)
-class BenchmarkResult:
-    title: str
-    benchmark: Benchmark
-
-    @property
-    def row(self) -> tuple[str, str, str, str]:
-        rate = self.benchmark.difference_rate
-        return (
-            self.title,
-            f"{self.benchmark.x:.2f}μs",
-            f"{self.benchmark.y:.2f}μs",
-            f"{rate:.2f}% slower" if rate >= 0 else f"{abs(rate):.2f}% faster",
-        )
-
-
-def inject_benchmark(
-    title: str,
-    function: Callable[..., Any],
-    dependencies: Iterable[Callable[..., Any]] = (),
-    number: int = 1,
-) -> Iterator[BenchmarkResult]:
-    def reference():
-        return function(*(d() for d in dependencies))
-
-    first = Benchmark.compare(
-        reference,
-        lambda: inject(function)(),
-        number=number,
-    )
-    yield BenchmarkResult(f"{title} (first run)", first)
-
-    injected = inject(function)
-    injected()
-    instance = Benchmark.compare(
-        reference,
-        injected,
-        number=number,
-    )
-    yield BenchmarkResult(title, instance)
 
 
 cli = Typer()
@@ -139,46 +160,10 @@ cli = Typer()
 
 @cli.command()
 def main(number: Annotated[int, Option("--number", "-n", min=0)] = 1000):
-    results = map(
-        lambda args: inject_benchmark(*args, number=number),
-        (
-            (
-                "0 dependency",
-                with_0_dependency,
-                (),
-            ),
-            (
-                "1 dependency",
-                with_1_dependency,
-                (A,),
-            ),
-            (
-                "2 dependencies",
-                with_2_dependencies,
-                (A, B),
-            ),
-            (
-                "3 dependencies",
-                with_3_dependencies,
-                (A, B, C),
-            ),
-            (
-                "4 dependencies",
-                with_4_dependencies,
-                (A, B, C, D),
-            ),
-            (
-                "5 dependencies",
-                with_5_dependencies,
-                (A, B, C, D, E),
-            ),
-        ),
-    )
-
+    results = InjectBenchmark().start(number)
     headers = ("", "Reference Time (μs)", "@inject Time (μs)", "Difference Rate (%)")
-    data = tuple(result.row for result in itertools.chain.from_iterable(results))
+    data = (result.row for result in itertools.chain(results))
     table = tabulate(data, headers=headers)
-
     print(table)
 
 
