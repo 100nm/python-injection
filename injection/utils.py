174c198140b6bc4a1e1d2b11684b8720bb929e35
remimd <remimd@github.com>
2025-04-11 13:12:52 +0200

    feat: ✨ PythonModuleLoader

--- injection/utils.py
+++ injection/utils.py
@@ -1,15 +1,18 @@
+import itertools
 import sys
-from collections.abc import Callable, Collection, Iterator
+from collections.abc import Callable, Iterator, Mapping
+from dataclasses import dataclass, field
 from importlib import import_module
 from importlib.util import find_spec
+from os.path import isfile
 from pkgutil import walk_packages
+from types import MappingProxyType
 from types import ModuleType as PythonModule
-from typing import ContextManager
+from typing import ClassVar, ContextManager, Self
 
 from injection import Module, mod
-from injection import __name__ as injection_package_name
 
-__all__ = ("load_modules_with_keywords", "load_packages", "load_profile")
+__all__ = ("PythonModuleLoader", "load_packages", "load_profile")
 
 
 def load_profile(*names: str) -> ContextManager[Module]:
@@ -21,41 +24,6 @@ def load_profile(*names: str) -> ContextManager[Module]:
     return mod().load_profile(*names)
 
 
-def load_modules_with_keywords(
-    *packages: PythonModule | str,
-    keywords: Collection[str] | None = None,
-) -> dict[str, PythonModule]:
-    """
-    Function to import modules from a Python package if one of the keywords is contained in the Python script.
-    The default keywords are:
-        - `from injection `
-        - `from injection.`
-        - `import injection`
-    """
-
-    if keywords is None:
-        keywords = (
-            f"from {injection_package_name} ",
-            f"from {injection_package_name}.",
-            f"import {injection_package_name}",
-        )
-
-    def predicate(module_name: str) -> bool:
-        spec = find_spec(module_name)
-
-        if spec and (module_path := spec.origin):
-            with open(module_path, "r") as file:
-                python_script = file.read()
-
-            return bool(python_script) and any(
-                keyword in python_script for keyword in keywords
-            )
-
-        return False
-
-    return load_packages(*packages, predicate=predicate)
-
-
 def load_packages(
     *packages: PythonModule | str,
     predicate: Callable[[str], bool] = lambda module_name: True,
@@ -65,32 +33,98 @@ def load_packages(
     Pass the `predicate` parameter if you want to filter the modules to be imported.
     """
 
-    loaded: dict[str, PythonModule] = {}
+    return PythonModuleLoader(predicate).load(*packages).modules
 
-    for package in packages:
+
+@dataclass(repr=False, eq=False, frozen=True, slots=True)
+class PythonModuleLoader:
+    predicate: Callable[[str], bool]
+    __modules: dict[str, PythonModule | None] = field(
+        default_factory=dict,
+        init=False,
+    )
+
+    # To easily mock `sys.modules` in tests
+    _sys_modules: ClassVar[Mapping[str, PythonModule]] = MappingProxyType(sys.modules)
+
+    @property
+    def modules(self) -> dict[str, PythonModule]:
+        return {
+            name: module
+            for name, module in self.__modules.items()
+            if module is not None
+        }
+
+    def load(self, *packages: PythonModule | str) -> Self:
+        modules = itertools.chain.from_iterable(
+            self.__iter_modules(package) for package in packages
+        )
+        self.__modules.update(modules)
+        return self
+
+    def __is_already_loaded(self, module_name: str) -> bool:
+        return any(
+            module_name in modules for modules in (self.__modules, self._sys_modules)
+        )
+
+    def __iter_modules(
+        self,
+        package: PythonModule | str,
+    ) -> Iterator[tuple[str, PythonModule | None]]:
         if isinstance(package, str):
             package = import_module(package)
 
-        loaded |= __iter_modules_from(package, predicate)
+        package_name = package.__name__
 
-    return loaded
+        try:
+            package_path = package.__path__
+        except AttributeError as exc:
+            raise TypeError(f"`{package_name}` isn't Python package.") from exc
 
+        for info in walk_packages(path=package_path, prefix=f"{package_name}."):
+            name = info.name
 
-def __iter_modules_from(
-    package: PythonModule,
-    predicate: Callable[[str], bool],
-) -> Iterator[tuple[str, PythonModule]]:
-    package_name = package.__name__
+            if info.ispkg or self.__is_already_loaded(name):
+                continue
 
-    try:
-        package_path = package.__path__
-    except AttributeError as exc:
-        raise TypeError(f"`{package_name}` isn't Python package.") from exc
+            module = import_module(name) if self.predicate(name) else None
+            yield name, module
 
-    for info in walk_packages(path=package_path, prefix=f"{package_name}."):
-        name = info.name
+    @classmethod
+    def from_keywords(cls, *keywords: str) -> Self:
+        """
+        Create loader to import modules from a Python package if one of the keywords is
+        contained in the Python script.
+        """
 
-        if info.ispkg or name in sys.modules or not predicate(name):
-            continue
+        def predicate(module_name: str) -> bool:
+            spec = find_spec(module_name)
 
-        yield name, import_module(name)
+            if spec is None:
+                return False
+
+            module_path = spec.origin
+
+            if module_path is None or not isfile(module_path):
+                return False
+
+            with open(module_path, "r") as script:
+                return any(keyword in line for line in script for keyword in keywords)
+
+        return cls(predicate)
+
+    @classmethod
+    def startswith(cls, *prefixes: str) -> Self:
+        def predicate(module_name: str) -> bool:
+            script_name = module_name.split(".")[-1]
+            return any(script_name.startswith(prefix) for prefix in prefixes)
+
+        return cls(predicate)
+
+    @classmethod
+    def endswith(cls, *suffixes: str) -> Self:
+        def predicate(module_name: str) -> bool:
+            script_name = module_name.split(".")[-1]
+            return any(script_name.endswith(suffix) for suffix in suffixes)
+
+        return cls(predicate)
