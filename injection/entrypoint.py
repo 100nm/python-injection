d55bc5591c331615f8b0fb08b7903ac322590ff2
remimd <remimd@github.com>
2025-05-22 20:32:39 +0200

    fix: 🐛 Don't call `ProfileLoader.init` if not required

--- injection/entrypoint.py
+++ injection/entrypoint.py
@@ -153,7 +153,9 @@ class Entrypoint[**P, T]:
         setup_method = profile_loader.module.make_injected_function(setup_method)
 
         def decorator(function: Callable[P, T]) -> Callable[P, _T]:
-            profile_loader.init()
+            if profile_loader.module_subsets:
+                profile_loader.init()
+
             self = cls(function, profile_loader)
             return MethodType(setup_method, self)().function
 
