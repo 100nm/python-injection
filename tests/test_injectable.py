0a4f63efc7484e762419d997e56a6bbc41154082
Rémi <78747335+remimd@users.noreply.github.com>
2025-06-12 10:10:51 +0200

    feat: ✨ Better support for `TypeAliasType`

--- tests/test_injectable.py
+++ tests/test_injectable.py
@@ -194,3 +194,24 @@ class TestInjectable:
 
         a = get_instance(A)
         assert isinstance(a, A)
+
+    def test_injectable_with_type_alias_type(self):
+        @injectable
+        class A: ...
+
+        type Alias1 = A
+        type Alias2 = Alias1
+
+        a1 = get_instance(Alias1)
+        a2 = get_instance(Alias2)
+        assert isinstance(a1, A)
+        assert isinstance(a2, A)
+
+    def test_injectable_with_generic_type_alias_type(self):
+        @injectable
+        class A[T]: ...
+
+        type Alias[T] = A[T]
+
+        a = get_instance(Alias[int])
+        assert isinstance(a, A)
