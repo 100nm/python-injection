2053ab5f079253c324b82822d4549bf647d3a9c1
remimd <remimd@github.com>
2025-09-24 11:35:34 +0200

    feat: ✨ PYTHON_INJECTION_THREADSAFE env var

--- injection/_core/common/threading.py
+++ injection/_core/common/threading.py
@@ -1,7 +1,11 @@
 from contextlib import nullcontext
+from os import getenv
 from threading import RLock
-from typing import Any, ContextManager
+from typing import Any, ContextManager, Final
+
+_PYTHON_INJECTION_THREADSAFE: Final[bool] = bool(getenv("PYTHON_INJECTION_THREADSAFE"))
 
 
 def get_lock(threadsafe: bool | None = None) -> ContextManager[Any]:
-    return RLock() if threadsafe else nullcontext()
+    cond = _PYTHON_INJECTION_THREADSAFE if threadsafe is None else threadsafe
+    return RLock() if cond else nullcontext()
